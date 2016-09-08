df88ba88c6ada0c61a220a6fd13cd7d297f8e57e
minoru-github <kurami0621@yahoo.co.jp>
2016-09-08 23:42:09 +0900

    ミドルウェアのバグ修正とコメント文を親切にした(つもり)
    バグ内容と修正内容
    1. sensorinfo()
    　f.readline()で取得できるのが配列形式と勘違いしていたがただの文字列だった。
    　取得した結果を" "区切りで取得し配列化するように変更
    2. buzzer()
    　デバドラに書き込むデータが整数型になっていたので文字列型に変更
    3. motor()
    　デバドラに書き込むデータが整数型になっていたので文字列型に変更

--- middleware.py
+++ middleware.py
@@ -10,55 +10,48 @@ buzzer_driver  = "/dev/rtbuzzer0"
 motor_driver   = [ "/dev/rtmotor_raw_l0", "/dev/rtmotor_raw_r0"]
 led_driver     = [ "/dev/rtled0", "/dev/rtled1", "/dev/rtled2", "/dev/rtled3"] 
 # デバイスドライバから取得した値を格納する配列
-sensor_data  = [0, 0, 0, 0] # [距離センサー0の取得値, 1の値, 2の値, 3の値]  
-switch_state = [0, 0, 0]    # [タクトスイッチ0の取得値, 1の値, 2の値]
+sensor_info  = [ 0, 0, 0, 0 ] # [ 距離センサー0の取得値, 1の値, 2の値, 3の値 ]  
+switch_state = [ 0, 0, 0 ]    # [ タクトスイッチ0の取得値, 1の値, 2の値 ]
 
 #-----------------------------------------------------------------------------#
 # Input system (入力系)                                                       #
 #-----------------------------------------------------------------------------#
 def sensorinfo():
     # 距離センサの値を取得する
-    # デバイスドライバが見つかった場合 => tryの処理
-    # デバイスドライバが見つからなかった場合 => exceptの処理
+    # デバイスドライバに正常にアクセスできた場合 => tryの処理
+    # デバイスドライバにアクセスできなかった場合 => exceptの処理
+    # （正確には、tryの処理中にエラーが発生した場合にexceptの処理が実行される）
     try:
         filename = sensor_driver
-        with open(filename,"r") as f:
-            # f.readlin() の返り値は 8要素の配列データ(すべての要素は文字列)
-            # 要素0 = 距離センサ0の取得値(文字列)
-            # 要素1 = スペース(文字列)
-            # 要素2 = 距離センサ1の取得値(文字列)
-            # 要素3 = スペース(文字列)
-            # 要素4 = 距離センサ2の取得値(文字列)
-            # 要素5 = スペース(文字列)
-            # 要素6 = 距離センサ3の取得値(文字列)
-            # 要素7 = \n(改行コード)
+        with open( filename, "r" ) as f:
+            # f.readlin() の返り値は文字列
+            # "距離センサー0の値" + " " + "1の値" + " " + "2の値" + " " + "3の値" + "\n"
             temp = f.readline()
+            # 取得した文字列をスペース区切りで分割する
+            temp = temp.split(" ")
             # デバイスドライバから取得できる値は文字列なので数値に変換する
-            # 要素1,要素3, 要素5のスペースを削除
-            # 要素7の改行コードを削除
-            info = [int(temp[0]), int(temp[2]), int(temp[4]), int(temp[6])]
+            info = [ int( temp[0] ), int( temp[1] ), int( temp[2] ), int( temp[3] ) ]
     except:
-        # デバイスドライバが見つからない場合は全ての要素に数値の255を代入する
+        # エラー発生時は全ての要素に数値の255を代入する
         info = [ 255, 255, 255, 255 ]
     sensor_info = info 
     return sensor_info
 
 def switchstate():
     # タクトスイッチの値を取得する
-    # デバイスドライバが見つかった場合 => tryの処理
-    # デバイスドライバが見つからなかった場合 => exceptの処理
+    # デバイスドライバに正常にアクセスできた場合 => tryの処理
+    # デバイスドライバにアクセスできなかった場合 => exceptの処理
+    # （正確には、tryの処理中にエラーが発生した場合にexceptの処理が実行される）
     try:
-        for i, filename in enumerate(switch_driver):
-            with open(filename,"r") as f:
-                # f.readlin() の返り値は 2要素の配列データ(すべての要素は文字列)
-                # 要素0 = "0":押されてない or "1":押されている
-                # 要素1 = \n(改行コード)
+        for i, filename in enumerate( switch_driver ):
+            with open( filename, "r" ) as f:
+                # f.readlin() の返り値は文字列)
+                # "0\n":押されてない or "1\n":押されている
                 temp = f.readline()
                 # デバイスドライバから取得できる値は文字列なので数値に変換する
-                # 要素1は改行コードなので削除
-                state[i] = int(temp[0])
+                state[i] = int( temp[0] )
     except:
-        # デバイスドライバが見つからない場合は全ての要素に数値の-1を代入する
+        # エラー発生時は全ての要素に数値の-1を代入する
         state = [ -1, -1, -1 ]
     switch_state = state
     return switch_state
@@ -66,45 +59,51 @@ def switchstate():
 #-----------------------------------------------------------------------------#
 # Output system (出力系)                                                      #
 #-----------------------------------------------------------------------------#
-def buzzer(frequency):
+def buzzer( frequency ):
     # ブザーを鳴らす
     # 引数は鳴らす音の周波数(Hz)
-    # デバイスドライバが見つかった場合 => tryの処理
-    # デバイスドライバが見つからなかった場合 => exceptの処理
+    # デバイスドライバに正常にアクセスできた場合 => tryの処理
+    # デバイスドライバにアクセスできなかった場合 => exceptの処理
+    # （正確には、tryの処理中にエラーが発生した場合にexceptの処理が実行される）
     try:
         filename = buzzer_driver
-        with open(filename,"w") as f:
-            f.write(int(frequency))
+        with open( filename, "w" ) as f:
+            f.write( str( int( frequency ) ) )
         ret = True
     except:
+        # エラー発生時は返値としてFalseを返す
         ret = False
     return ret
 
-def motor(speed):
+def motor( speed ):
     # モーターを駆動する
     # 引数は配列データ( speed = [ 左モータの回転数(Hz), 右モータの回転数(Hz) ] )
-    # デバイスドライバが見つかった場合 => tryの処理
-    # デバイスドライバが見つからなかった場合 => exceptの処理
+    # デバイスドライバに正常にアクセスできた場合 => tryの処理
+    # デバイスドライバにアクセスできなかった場合 => exceptの処理
+    # （正確には、tryの処理中にエラーが発生した場合にexceptの処理が実行される）
     try:
-        for i, filename in enumerate(motor_driver):
-            with open(filename,"w") as f:
-                f.write(str(int(speed[i])))
+        for i, filename in enumerate( motor_driver ):
+            with open( filename, "w" ) as f:
+                f.write( str( int( speed[i] ) ) )
         ret = True
     except:
+        # エラー発生時は返値としてFalseを返す
         ret = False
     return ret
 
-def led(led_state):
+def led( led_state ):
     # LEDを駆動する
     # 引数は配列データ( led_state = [ led_0, led_1, led_2,led_3 ] )
     # "led_X = 0" => Off , "led_X = 1" => ON
-    # デバイスドライバが見つかった場合 => tryの処理
-    # デバイスドライバが見つからなかった場合 => exceptの処理
+    # デバイスドライバに正常にアクセスできた場合 => tryの処理
+    # デバイスドライバにアクセスできなかった場合 => exceptの処理
+    # （正確には、tryの処理中にエラーが発生した場合にexceptの処理が実行される）
     try:
-        for i,filename in enumerate(led_driver):
-            with open(filename,'w') as f: 
-                f.write(led_state[i])
+        for i,filename in enumerate( led_driver ):
+            with open( filename, 'w' ) as f: 
+                f.write( str( led_state[i] ) )
         ret = True
     except:
+        # エラー発生時は返値としてFalseを返す
         ret = False
     return ret
