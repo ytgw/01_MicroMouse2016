8784d4d98a819dde4756777d38d80e8a5780b951
tagawa <tagawa0920@gmail.com>
2017-07-03 21:07:58 +0900

    newSequenceの変更(デバッグ済み)

--- newSequence.py
+++ newSequence.py
@@ -16,8 +16,8 @@ import recognition as rcg
 import action as act
 import stop as stop
 import userutility as utl
-import copy
 import sys
+
 #-----------------------------------------------------------------------------#
 # Declation                                                                  #
 #-----------------------------------------------------------------------------#
@@ -27,11 +27,24 @@ GLOBAL_INITIAL_MODE = 0    # 初期状態
 GLOBAL_SEARCH_MODE  = 1    # 探索状態
 GLOBAL_FAST_MODE    = 2    # 最速走行状態
 GLOBAL_GOAL_MODE    = 3    # ゴール状態
+
 # 方向
-GLOBAL_EAST         = 1
-GLOBAL_NORTH        = 2
-GLOBAL_WEST         = 3
-GLOBAL_SOUTH        = 4
+GLOBAL_EAST  = 1
+GLOBAL_NORTH = 2
+GLOBAL_WEST  = 3
+GLOBAL_SOUTH = 4
+
+# 壁情報のビット表現
+RIGHT  = 0b00000001
+TOP    = 0b00000010
+LEFT   = 0b00000100
+BOTTOM = 0b00001000
+
+# 距離情報取得用
+FRONT_DIRECTION = 0     # 前方
+LEFT_DIRECTION  = 1     # 左方
+RIGHT_DIRECTION = 2     # 右方
+
 # 初期状態
 GLOBAL_INITIAL_POSITION  = [0,0]
 GLOBAL_INITIAL_WALL_INFO = 13
@@ -41,39 +54,69 @@ GLOBAL_INITIAL_DIRECTION = GLOBAL_NORTH
 global_maze      = utl.Maze()                   # 迷路インスタンス
 global_position  = GLOBAL_INITIAL_POSITION      # 現在座標
 global_direction = GLOBAL_INITIAL_DIRECTION     # 現在の方向
-global_was_moved = True                         # 区画移動が終了時はTrue
 
-#タクトスイッチ押した判定用のカウンタ
-swstatecnt = [0,0,0]
-
-# 壁情報のビット表現
-RIGHT  = 0b00000001
-TOP    = 0b00000010
-LEFT   = 0b00000100
-BOTTOM = 0b00001000
-
-# 距離情報取得用
-FRONT_DIRECTION     = 0             # 前方
-LEFT_DIRECTION      = 1             # 左方
-RIGHT_DIRECTION     = 2             # 右方
-
-ROTATERIGHT = -88
-ROTATELEFT = 88
-ROTATEOPPOSIT = 175
 
 #-----------------------------------------------------------------------------#
 # Functions                                                                  #
 #-----------------------------------------------------------------------------#
+def main():
+    '''
+    マイクロマウスマシンの起動プログラム関数
+    '''
+    global global_position, global_direction
+
+    print "Main function is called"
+    mode = GLOBAL_INITIAL_MODE
+    
+    while True:
+        # 状態の遷移と表示
+        presentMode = mode
+        mode = selectMode(presentMode, global_position, utl.goal)
+        if presentMode != mode:
+            if mode == GLOBAL_INITIAL_MODE:
+                act.led([1,0,0,0])
+                print "Mode changed to INITIAL MODE"
+            elif mode == GLOBAL_SEARCH_MODE:
+                act.led([0,1,0,0])
+                global_position  = GLOBAL_INITIAL_POSITION
+                global_direction = GLOBAL_INITIAL_DIRECTION
+                print "Mode changed to SEARCH MODE"
+            elif mode == GLOBAL_FAST_MODE:
+                act.led([0,0,1,0])
+                global_position  = GLOBAL_INITIAL_POSITION
+                global_direction = GLOBAL_INITIAL_DIRECTION
+                print "Mode changed to FAST MODE"
+            elif mode == GLOBAL_GOAL_MODE:
+                act.led([0,0,0,1])
+                print "Mode changed to GOAL MODE"
+            else:
+                act.led([1,1,1,1])
+                print "Mode changed to UNDEFINED MODE"
+        
+        # 状態ごとの動作切り替え
+        if mode == GLOBAL_INITIAL_MODE:
+            initialModeFunction()
+        elif mode == GLOBAL_SEARCH_MODE:
+            searchModeFunction()
+        elif mode == GLOBAL_FAST_MODE:
+            fastModeFunction()
+        elif mode == GLOBAL_GOAL_MODE:
+            goalModeFunction()
+        else:
+            act.led([1,1,1,1])
+            print "You are in undefined mode"
+
 
 def selectMode(presentMode, presentPosition, goalPositions):
     '''
-    状態遷移関数selectModeの概要
+    状態遷移関数
     入力:現在の状態，現在の位置，ゴール位置のリスト
     出力:遷移後の状態
     タクトスイッチ0を押すと初期状態へ遷移
     タクトスイッチ1を押すと初期状態のときは探索状態へ遷移するが，
     ゴール状態のときは最速走行状態へ遷移し，それ以外の状態では遷移なし
     タクトスイッチ2を押すとプログラム終了
+    現在座標がゴール座標に含まれていたらゴール状態に遷移
     '''
     mode = presentMode
 
@@ -81,14 +124,14 @@ def selectMode(presentMode, presentPosition, goalPositions):
     swState = rcg.get_switch_state()
     
     # スイッチ情報による遷移
-    if swState[0] == 1:
+    if swState[0] == 0:
         mode = GLOBAL_INITIAL_MODE
-    elif swState[1] == 1:
+    elif swState[1] == 0:
         if presentMode == GLOBAL_INITIAL_MODE:
             mode = GLOBAL_SEARCH_MODE
         elif presentMode == GLOBAL_GOAL_MODE:
             mode = GLOBAL_FAST_MODE
-    elif swState[2] == 1:
+    elif swState[2] == 0:
         print "Sequence is killed by using switch"
         act.led([0,0,0,0])
         act.buzzer(0)
@@ -101,8 +144,14 @@ def selectMode(presentMode, presentPosition, goalPositions):
     
     return mode
 
+
 def rcgWall2mazeWall(presentDirection,rcgWallInfo):
-    directionForConvert = TOP
+    '''
+    認知壁情報から迷路用壁情報への変換関数
+    入力:現在の方向，認知モジュールからの方向補正なし壁情報
+    出力:方向を補正した迷路用壁情報
+    change_the_worldのラッパー
+    '''
     if presentDirection == GLOBAL_EAST:
         directionForConvert = RIGHT
     elif presentDirection == GLOBAL_NORTH:
@@ -112,13 +161,19 @@ def rcgWall2mazeWall(presentDirection,rcgWallInfo):
     elif presentDirection == GLOBAL_SOUTH:
         directionForConvert = BOTTOM
     else:
+        directionForConvert = TOP
         print "An error occurs in rcgWall2mazeWall function"
     
-    change_the_world(directionForConvert,rcgWallInfo)
+    mazeWallInfo = change_the_world(directionForConvert,rcgWallInfo)
     return mazeWallInfo
 
 
 def change_the_world(mydirection, local_wallinfo):
+    '''
+    認知壁情報から迷路用壁情報への変換関数
+    入力:現在の方向，認知モジュールからの方向補正なし壁情報
+    出力:方向を補正した迷路用壁情報
+    '''
     global_wallinfo = 0
     #print "mydirectionC=",mydirection
     #print "local_wallinfoC",local_wallinfo
@@ -155,58 +210,15 @@ def change_the_world(mydirection, local_wallinfo):
     return global_wallinfo
 
 
-def main():
-    '''
-    マイクロマウスマシンの起動プログラム関数
-    入出力なし
-    '''
-    global global_position
-
-    print "Main function is called"
-    mode = GLOBAL_INITIAL_MODE
-    
-    while True:
-        # 状態ごとの動作切り替え
-        if mode == GLOBAL_INITIAL_MODE:
-            initialModeFunction()
-        elif mode == GLOBAL_SEARCH_MODE:
-            searchModeFunction()
-        elif mode == GLOBAL_FAST_MODE:
-            fastModeFunction()
-        elif mode == GLOBAL_GOAL_MODE:
-            goalModeFunction()
-        else:
-            act.led([1,1,1,1])
-            print "You are in undefined mode"
-
-        # 状態の遷移と表示
-        presentMode = mode
-        mode = selectMode(presentMode, global_position, utl.goal)
-        if presentMode != mode:
-            if mode == GLOBAL_INITIAL_MODE:
-                act.led([1,0,0,0])
-                print "Mode changed to INITIAL MODE"
-            elif mode == GLOBAL_SEARCH_MODE:
-                act.led([0,1,0,0])
-                print "Mode changed to SEARCH MODE"
-            elif mode == GLOBAL_FAST_MODE:
-                act.led([0,0,1,0])
-                print "Mode changed to FAST MODE"
-            elif mode == GLOBAL_GOAL_MODE:
-                act.led([0,0,0,1])
-                print "Mode changed to GOAL MODE"
-            else:
-                act.led([1,1,1,1])
-                print "Mode changed to UNDEFINED MODE"
-
-
-#--------------------------------------------------------------#
-# 初期状態の動作関数
-#--------------------------------------------------------------#
 def initialModeFunction():
+    '''
+    初期状態の動作関数
+    '''
     global global_maze, global_position, global_direction
     # モータへの停止命令
     stop.stop()
+    # LEDの表示
+    act.led([1,0,0,0])
     # 迷路インスタンスのリセット
     global_maze = utl.Maze()
     # 座標のリセット
@@ -214,18 +226,32 @@ def initialModeFunction():
     # 方向のリセット
     global_direction = GLOBAL_INITIAL_DIRECTION
 
-#--------------------------------------------------------------#
-# 探索状態の動作関数
-#--------------------------------------------------------------#
+
 def searchModeFunction():
+    '''
+    探索走行状態の動作関数
+    '''
     global global_maze, global_position, global_direction
+
+    # デバッグ用表示
+    print "Present position  is (%d, %d)" % (global_position[0], global_position[1])
+    if global_direction == GLOBAL_EAST:
+        directionStr = "East"
+    elif global_direction == GLOBAL_NORTH:
+        directionStr = "North"
+    elif global_direction == GLOBAL_WEST:
+        directionStr = "West"
+    elif global_direction == GLOBAL_SOUTH:
+        directionStr = "South"
+    else:
+        directionStr = "undefined direction"
+    print "Present direction is " + directionStr
+    print "----------"
+    
     if global_position == GLOBAL_INITIAL_POSITION:
         # 最初は半歩前進後に座標更新
-        half_step()     # half_stepは動作終了まで処理を専有する関数
+        goRotateGo(0,0,0.6)     # goRotateGoは動作終了まで処理を専有する関数
         global_position = [0,1]
-    elif global_position == utl.goal:
-        # 最後は半歩前進
-        half_step()     # half_stepは動作終了まで処理を専有する関数
     else:
         # 壁情報のセット
         wallInfoFromRcg = rcg.check_wall()
@@ -235,39 +261,47 @@ def searchModeFunction():
         global_maze.adachi()
         nextPosition = global_maze.get_nextpos(global_position)
         # blockMoveは動作終了まで処理を専有する関数
-        global_direction = blockMove(global_position, nextPosition, global_direction)
+        returnedDirection = blockMove(global_position, nextPosition, global_direction)
+        global_direction  = returnedDirection
         # 移動命令終了後は座標更新
         global_position = nextPosition
+        if global_position in utl.goal:
+            goRotateGo(0,0,0.5)     # goRotateGoは動作終了まで処理を専有する関数
 
-#--------------------------------------------------------------#
-# 最速走行状態の動作関数
-#--------------------------------------------------------------#
+
 def fastModeFunction():
+    '''
+    最速走行状態の動作関数
+    '''
     # 最初の処理
     # 最後の処理
     # 最初と最後以外の処理
     print "FAST MODE is not implemented"
 
-#--------------------------------------------------------------#
-# ゴール状態の動作関数
-#--------------------------------------------------------------#
+
 def goalModeFunction():
+    '''
+    ゴール状態の動作関数
+    '''
     # モータへの停止命令
     stop.stop()
 
-#--------------------------------------------------------------#
-# ブロック移動関数
-#--------------------------------------------------------------#
+
 def blockMove(presentPosition, nextPosition, presentDirection):
     '''
-    入力:現在座標，移動後座標，現在方向
+    ブロック移動関数
+    入力:現在座標，移動先座標，現在方向
     出力:移動後方向
+    現在座標と移動先座標をもとに移動する
+    ただし，x方向とy方向を同時に移動させることはできない
+    関数が終了するまで，他の処理は実施できない
     '''
     xDist = nextPosition[0] - presentPosition[0]
     yDist = nextPosition[1] - presentPosition[1]
+    nextDirection = presentDirection
+
     if (xDist != 0) and (yDist != 0):
         print "An error occurs in blcokMove function"
-        nextDirection = presentDirection
     else:
         # x方向の移動
         if xDist != 0:
@@ -291,11 +325,8 @@ def blockMove(presentPosition, nextPosition, presentDirection):
                     goRotateGo(0,0,1)
                 elif presentDirection == GLOBAL_SOUTH:
                     goRotateGo(0.5,-90,0.5)
-        else:
-            nextDirection = presentDirection
         
         # y方向の移動
-        presentDirection = nextDirection
         if yDist != 0:
             if yDist > 0:
                 nextDirection = GLOBAL_NORTH
@@ -317,16 +348,18 @@ def blockMove(presentPosition, nextPosition, presentDirection):
                     goRotateGo(0.5,90,0.5)
                 elif presentDirection == GLOBAL_SOUTH:
                     goRotateGo(0,0,1)
-        else:
-            nextDirection = presentDirection
     
     return nextDirection
 
 
-#--------------------------------------------------------------#
-# 直進走行+回転走行+直進走行関数
-#--------------------------------------------------------------#
 def goRotateGo(blockDistance1,degreeAngle,blockDistance2):
+    '''
+    直進走行+回転走行+直進走行関数
+    入力:直進距離1[block]，回転角[degree]，直進距離2[block]
+    出力:なし
+    直進距離1分だけ直進後，回転角分だけ回転，その後直進距離2分だけ直進する
+    関数が終了するまで，他の処理は実施できない
+    '''
     # 直進1
     dist = rcg.get_distance()
     dist[FRONT_DIRECTION] = -1
@@ -351,25 +384,5 @@ def goRotateGo(blockDistance1,degreeAngle,blockDistance2):
         act.keep_order(dist[FRONT_DIRECTION],dist[LEFT_DIRECTION],dist[RIGHT_DIRECTION])
 
 
-#--------------------------------------------------------------#
-#--------------------------------------------------------------#
-def half_step():
-    distance = rcg.get_distance()
-    distance[FRONT_DIRECTION] = -1
-##  distance[LEFT_DIRECTION] = -1
-##  distance[RIGHT_DIRECTION] = -1
-    # 0.5マス前進
-    act.go_straight(0.60,distance[FRONT_DIRECTION],distance[LEFT_DIRECTION],distance[RIGHT_DIRECTION])
-    # 0.5マス進み終わるまで待機
-    while True:
-        distance = rcg.get_distance()
-        distance[FRONT_DIRECTION] = -1
-        act.keep_order(distance[FRONT_DIRECTION],distance[LEFT_DIRECTION],distance[RIGHT_DIRECTION])
-        if not(act.is_running()):
-            break
-    return True
-
-
 if __name__ == '__main__':
     main()
-
