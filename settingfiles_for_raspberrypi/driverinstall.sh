68409af30f717697cb60c6b9f038a7d2f6a8f75a
ytgw <tagawa@y-tagawas-MBA.local>
2017-11-18 15:26:38 +0900

    コミット漏れをコミット

--- settingfiles_for_raspberrypi/driverinstall.sh
+++ settingfiles_for_raspberrypi/driverinstall.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sudo insmod /home/pi/deviceDriverFromGitHub/lib/Pi2B+/4.9.35-v7+/rtmouse.ko
+sudo insmod /home/pi/Desktop/ShellScript/deviceDriverFromGitHub/lib/Pi2B+/4.9.35-v7+/rtmouse.ko
 sudo chmod 666 /dev/rt*
